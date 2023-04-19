fbc14cbc01b2bcef6626f9087a53eb8f2ee91fce
Matheus-Ghizoni <matheus.ghizoni24@gmail.com>
2023-04-19 20:37:20 -0300

    Fix: OT15 Finalizada
    Durante está OT foi realizado a implementação da página de registro de produtos, aplicando o HTML e o CSS da página.
    
    Matheus Ghizoni

--- ProjetoTrilhaWeb/WebContent/js/admin.js
+++ ProjetoTrilhaWeb/WebContent/js/admin.js
@@ -1,6 +1,18 @@
+COLDIGO = new Object();
+
 $(document).ready(function() {
 
     $("header").load("/ProjetoTrilhaWeb/pages/admin/general/header.html");
     $("footer").load("/ProjetoTrilhaWeb/pages/admin/general/footer.html");
-  
+
+    COLDIGO.carregaPagina = function(pagename){
+        $("section").empty();
+        $("section").load(pagename+"/", function(status, info){
+            if (status == "error") {
+                var msg = "Houve um erro ao encontrar a página: "+ info.status + "  -  " + info.statusText;
+                $("section").html(msg);       
+            }
+        });
+    }
+
 });
